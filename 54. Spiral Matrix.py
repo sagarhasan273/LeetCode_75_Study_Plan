a08493157e6ceb881d8f6e84a40bfc839fb81cdf
sagarhasan273 <sagarhasan273@gmail.com>
2022-07-29 17:38:49 +0600

    50% plus runtime

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -1,25 +1,26 @@
 class Solution:
-    def findBall(self, grid: List[List[int]]) -> List[int]:
-        rows, cols = len(grid), len(grid[0])
-        
-        def dfs(r, c):
-            if r == rows-1:
-                if grid[r][c] == 1 and c+1 < cols and grid[r][c] == grid[r][c+1]:
-                    return c + grid[r][c]
-                if grid[r][c] == -1 and c-1 > -1 and grid[r][c] == grid[r][c-1]:
-                    return c + grid[r][c]
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        rows, cols = len(matrix), len(matrix[0])
+        r, c = 0, 0
+        res = []
+        while r < rows and c < cols:
 
-                return -1
+            for j in range(c, cols):
+                res.append(matrix[r][j])
+            r += 1
             
-            elif grid[r][c] == 1 and c+1 < cols and grid[r][c] == grid[r][c+1]:
-                return dfs(r+1, c+1)
-            elif grid[r][c] == -1 and c-1 > -1 and grid[r][c] == grid[r][c-1]:
-                return dfs(r+1, c-1)
-            else:
-                return -1
-        
-        ans = []
-        for c in range(cols):
-            ans.append(dfs(0, c))
-        
-        return ans
+            for i in range(r, rows):
+                res.append(matrix[i][cols-1])
+            cols -= 1
+            
+            if r < rows:
+                for j in range(cols-1, c-1, -1):
+                    res.append(matrix[rows-1][j])
+                rows -= 1
+            
+            if c < cols:
+                for i in range(rows-1, r-1, -1):
+                    res.append(matrix[i][c])
+                c += 1
+            
+        return res
